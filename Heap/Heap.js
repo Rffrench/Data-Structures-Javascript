3b6aba3dab8c3db8b12067f7fa7e9ffa29cabdc3
Roberto Ffrench-Davis <roberto_ffrench@hotmail.com>
2020-06-26 02:25:26 -0400

    Heap done

--- Heap/Heap.js
+++ Heap/Heap.js
@@ -37,9 +37,73 @@ You should use heap when you require to allocate a large block of memory. For ex
 
 However, If you are working with relatively small variables that are only required until the function using them is alive. Then you need to use the stack, which is faster and easier.
 
+
+*** WHAT IS A PERMUTATION? ***
+
+A Permutation of a collection of elements is the list of all the possible ways you can order the elements. All the possible combinations
+E.g: [1,2,3] = [1,2,3] [1,3,2] [2,1,3] [2,3,1] [3,2,1] [3,1,2]
+
+The number of permutations 'n' can be calculated by the total count factorial n!.
+E.g: N = 3 >> 3*2*1 = 6 permutations (Factorial 3!)
+     N = 4 >> 4*3*2*1 = 24 permutations (Factorial 4!)
+
+*Heap's Algorithm is one way to get all these permutations
+
+
+*** NOTES ABOUT THE ACTUAL ALGORITHM ***
+
+- The algorithm is HARD and UNINTUITIVE. Probably around 99% of the programmers wouldn't be able to come with a solution by their own without looking at the code
+- This is a algorithm you study, not come up with on your own.
+- Recursion is something that is needed to understand it
+
+
+
 Reference:
 https://www.guru99.com/stack-vs-heap.html
 https://stackoverflow.com/questions/79923/what-and-where-are-the-stack-and-heap?rq=1
+https://www.youtube.com/watch?v=xghJNlMibX4   >>> This video thoroughly explains the syntax
  */
 
+// JAVASCRIPT SYNTAX
+
+
+// THis will get all the permutations as an array, each element will be another array of the permutations
+const getPermutations = arr => {
+
+
+    const output = []; // output that will have the arrays
+
+    // This function will swap the postion of an element of an array based on the indexes.
+    const swapInPlace = (arrToSwap, indexA, indexB) => {
+        const temp = arrToSwap[indexA];
+        arrToSwap[indexA] = arrToSwap[indexB];
+        arrToSwap[indexB] = temp;
+    }
+
+    // Generating the permutations. 'n' are the initial elements of the collection
+    const generate = (n, heapArr) => {
+        if (n === 1) {
+            output.push(heapArr.slice()); // The array is added to the output. slice() used to make a real copy of it
+            return;
+        } else {
+            generate(n - 1, heapArr); // Recursive function
+
+            for (let i = 0; i < n - 1; i++) {
+                if (n % 2 === 0) { // If it is even we swap the indexes of the array
+                    swapInPlace(heapArr, i, n - 1);
+                } else {
+                    swapInPlace(heapArr, 0, n - 1);
+                }
+
+                generate(n - 1, heapArr); // Recursion again
+            }
+        }
+    }
+
+    generate(arr.length, arr.slice()); // We don't want to manipulate the original array so with slice() we pass a real copy of it
+
+    return output;
+};
+
+console.log(getPermutations([1, 2, 3, 4]));
 
--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Data-Structures-Javascript
 A collection of small Javascript files of many Data Structures and their explanation.
+
+# ¿What are they?
+
+A data structure is a way of organizing data that is stored in a computer so that it can be used efficiently. They are different ways of storing data in computers
+
+
+# Memory
+It is a long tape of bytes. Each byte in the RAM is stored in an address. Applications will have an address for them 
+Each character is stored as 4 bytes or 32 bits long. (4 x 8 bits) byte = 8 bits.  
+Each integer, decimal or string will then take the same amount of Memory.
+
+
+# Recursion
+
+Recursion is a way of solving a problem by having a function call itself. 
+Example: factorial function. The function calls itself recursively but once it reaches the min value 1 it goes back again the tree to return the value.
+
+int fact(int n){
+    if(n >= 1){ return n * fact(n-1); } // This function will call itself everytime 
+    else { return 1; }
+}
+
+fact(4) -> 4 * fact(3)
+fact(3) -> 3 * fact(2)
+
+
+
+
+# Big O Notation
+
+ Big O notation is important because that will tell how long is an algorithm going to take to complete. Always remember is about Time and complexity
